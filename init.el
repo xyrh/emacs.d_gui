2683ac504932b3a3a310be6f5029a5e1f37b4c50
xyrh <o_rdwr@qq.com>
2019-07-17 20:47:59 +0800

    dired mode

--- init.el
+++ init.el
@@ -38,6 +38,8 @@
           (message file-name)
           (kill-new file-name)))))
 (global-set-key (kbd "C-c C-f") 'show-buffer-file-name)
+
+(put 'dired-find-alternate-file 'disabled nil)
 (with-eval-after-load 'dired
   (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
 
